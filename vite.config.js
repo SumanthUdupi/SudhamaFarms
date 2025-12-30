acb4e6ac1790cc7e86a6a9ac47012df7a7ac6966
Sumanth Udupi <109497622+SumanthUdupi@users.noreply.github.com>
2025-12-30 10:55:26 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,7 +24,7 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+      plugins: [tailwindcss, autoprefixer],
     },
   },
   resolve: {
